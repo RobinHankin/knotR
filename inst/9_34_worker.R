1a9d8ad9c8dd6d525b19caadd1205dfd7bff0f03
RobinHankin <hankin.robin@gmail.com>
2017-10-18 09:01:32 +1300

    minor tweaks

--- inst/9_34_worker.R
+++ inst/9_34_worker.R
@@ -1,24 +1,27 @@
 library(knotR)
 filename <- "9_34.svg"
 a <- reader(filename)
+##knotplot2(a,node=T)
 
 
 Mver934 <-
   matrix(c(
-      09,07,
-      18,27,
-      17,28,
-      16,29,
-      15,01,
-      10,06,
-      26,19,
-      11,05,
-      25,20,
-      24,21,
-      04,12,
-      03,13,
-      14,02,
-      23,22
+      10,08,
+      20,32,
+      19,33,
+      18,34,
+      17,01,
+      11,07,
+      12,06,
+      31,21,
+      13,05,
+      30,22,
+      29,23,
+      28,24,
+      04,14,
+      03,15,
+      16,02,
+      27,25
   ),byrow=TRUE,ncol=2)
 
 
@@ -26,27 +29,28 @@ sym934 <-
     symmetry_object(
         x    = a,
         Mver = Mver934,
-        xver = 8,
+        xver = c(9,26),
         mcdonalds=FALSE
         )
     
 a <- symmetrize(a,sym934)
-#knotplot2(a,circ=0,node=F,text=T,lwd=1,seg=F,rainbow=TRUE)
+##knotplot2(a,circ=0,node=F,text=T,lwd=1,seg=F,rainbow=TRUE)
 
 
 ou934  <-
   matrix(c(
-      02,22,
-      14,03,
-      05,12,
-      20,06,
-      07,28,
-      18,10,
-      11,26,
-      24,15,
-      27,19
+      02,25,
+      16,03,
+      05,14,
+      22,06,
+      08,33,
+      20,11,
+      13,31,
+      28,17,
+      32,21
   ),byrow=TRUE,ncol=2)
 
+knotplot(a,ou934)
 
 
 #knotplot(a,ou934)
@@ -56,7 +60,7 @@ jj <- knotoptim(filename,
                 ou      = ou934,
                 prob    = 0,
                 iterlim = 1000, print.level=2
-#                control=list(trace=100,maxit=10000), useNLM=FALSE
+#                control=list(trace=100,maxit=1000), useNLM=FALSE
                 )
 
 write_svg(jj,filename,safe=FALSE)
