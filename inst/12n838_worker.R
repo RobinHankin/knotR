216ac514b2fc939c87ce5c733d9173a01d6c9a16
RobinHankin <hankin.robin@gmail.com>
2017-10-19 21:13:42 +1300

    new nodes

--- inst/12n838_worker.R
+++ inst/12n838_worker.R
@@ -4,60 +4,58 @@ a <- reader(filename)
 #knotplot2(a,node=TRUE)
 
 Mver <- matrix(c(
-    35,01,
-    23,13,
-    22,14,
-    12,24,
-    11,25,
-    34,02,
-    26,10,
-    21,15,
-    27,09,
-    33,03,
-    28,08,
-    20,16,
-    32,04,
-    29,07,
-    30,06,
-    31,05,
-    19,17
+    37,01,
+    24,14,
+    23,15,
+    22,16,
+    36,02,
+    35,03,
+    13,25,
+    12,26,
+    27,11,
+    28,10,
+    34,04,
+    05,33,
+    29,09,
+    30,08,
+    21,17,
+    32,06,
+    31,07,
+    20,18
 ),ncol=2,byrow=TRUE)
 
-
-sym828 <- symmetry_object(a,Mver=Mver,xver=c(18,36))
+sym828 <- symmetry_object(a,Mver=Mver,xver=c(19,38))
 a <- symmetrize(a,sym828)
 #knotplot2(a,node=TRUE)
 
 
-#knotplot2(a,text=TRUE,lwd=1,circ=FALSE,rainbow=TRUE)
-
+knotplot2(a,text=TRUE,lwd=1,circ=FALSE,rainbow=TRUE)
 
 ou828 <- matrix(c(  # nonalternating!
-    02,14,
-    10,03,
-    04,33,
-    32,05,
-    06,17,
-    16,09,
-    26,11,
-    31,20,
-    21,28,
-    35,23,
-    13,24,
-    27,34
+    02,15,
+    11,04,
+    05,34,
+    33,06,
+    07,18,
+    17,10,
+    27,12,
+    14,25,
+    32,21,
+    22,29,
+    37,24,
+    28,35
 ),ncol=2,byrow=TRUE)
 
-#knotplot(a,ou828,gap=10)
+##knotplot(a,ou828,gap=10)
 
 jj <-
   knotoptim(filename,
             symobj = sym828,
             ou   = ou828,
             prob = 0,
-            ##            iterlim=1000, print.level=2,
-            control=list(trace=100,maxit=100000), useNLM=FALSE
+            iterlim=1000, print.level=2,
+#            control=list(trace=100,maxit=100000), useNLM=FALSE
             )
-
 
 write_svg(jj, filename,safe=FALSE)
 dput(jj,file=sub('.svg','.S',filename))
