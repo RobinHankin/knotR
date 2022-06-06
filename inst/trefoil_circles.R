9ad1c9402f2281daa3cfe5016210be492450d449
Robin Hankin <hankin.robin@gmail.com>
2022-06-06 17:40:23 +1200

    start work on #58

--- inst/trefoil_circles.R
+++ inst/trefoil_circles.R
@@ -5,11 +5,13 @@
 ## rotational symmetry.  Constraint #1 implies that the strands are
 ## arcs of a circle.
 
-## 
-
 ## Elementary trigonometry then shows that the radii of the inner and
 ## outer loops are cosec(pi/12) and sec(pi/12) = sqrt(6)+sqrt(2) and
 ## sqrt(6)-sqrt(2) respectively.
+
+## NB this file produces trefoil_circles.pdf, which is not production
+## quality!  It needs to be tidied up in inkscape before anyone sees
+## it.
 
 `arc` <- function(x,y,r,theta1,theta2,rot=0,center=FALSE, ...){
   n <- 100
@@ -21,16 +23,25 @@
   points(x+r*sin(theta),y+r*cos(theta),type='l',...)
 }
 
+small <- 0.05
+
 `f` <- function(angle,...){
-  arc(x=0, y=-2-2/sqrt(3), theta1 =    -pi/12, theta2 =   pi/12, r=sqrt(6)+sqrt(2), rot=angle,...)
-  arc(x=0, y=+2-2/sqrt(3), theta1 =  -7*pi/12, theta2 = 7*pi/12, r=sqrt(6)-sqrt(2), rot=angle,...)
+  arc(x=0, y=-2-2/sqrt(3), theta1 =   0, theta2 =   pi/12, r=sqrt(6)+sqrt(2), rot=angle,col='red',lwd=17,...)
+  arc(x=0, y=+2-2/sqrt(3), theta1 =  0, theta2 = 7*pi/12, r=sqrt(6)-sqrt(2), rot=angle,col='red',lwd=17,...)
+
+  arc(x=0, y=-2-2/sqrt(3), theta1 = pi/12-small, theta2 =   pi/12+small, r=sqrt(6)+sqrt(2), rot=angle,col='blue',lwd=39,...)
+
+  arc(x=0, y=-2-2/sqrt(3), theta1 =    -pi/12, theta2 =   0, r=sqrt(6)+sqrt(2), rot=angle,lwd=17,...)
+  arc(x=0, y=+2-2/sqrt(3), theta1 =  -7*pi/12, theta2 = 0, r=sqrt(6)-sqrt(2), rot=angle,lwd=17,...)
 }
 
-
+pdf(file="trefoil_circles.pdf",height=9,width=9)
 par(pty='s')
 u <- 1.9
 plot(NA,type='n',asp=1,xlim=c(-u,u),ylim=c(-u,u),axes=FALSE,xlab='',ylab='')
 
-f(0*pi/3,lwd=5)
-f(2*pi/3,lwd=5)
-f(4*pi/3,lwd=5)
+f(0*pi/3)
+f(2*pi/3)
+f(4*pi/3)
+
+dev.off()
