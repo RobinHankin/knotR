27802faeb108af25ee859eeaf5e0b847696ac846
Robin Hankin <hankin.robin@gmail.com>
2022-06-15 17:59:30 +1200

    slight visual improvement in crossing mechanism

--- inst/knotter.R
+++ inst/knotter.R
@@ -61,6 +61,39 @@ leg <- list(
     expression(8[21])
 )
     
+`knotplot_bespoke` <- function(x, ou, gapwidth=1, n=100, lwd=8, add=FALSE, ...){
+    if(inherits(x,'knot')){
+      ou <- x$overunderobj
+      x <- as.minobj(x)
+    }
+    stopifnot(is.sensible(ou,x))
+    a <- as.inkscape(x)
+    if(!add){
+      plot(a,type='n',asp=1, axes=FALSE, xlab='', ylab='', ...)
+    }
+
+    b <- as.controlpoints(a)
+
+    xy_thin  <- matrix(0,0,2)
+    xy_thick <- matrix(0,0,2)
+    for(i in seq_along(b)){  # loop over all Bezier segments
+      tee <- seq(from=0, to=1, len=n)  
+      overs <- ou[ou[,2]==i,1]  # strands that pass *over* strand i  (NB: might be empty!)
+      for(j in overs){  # loop over strands j that pass over strand i; i is under, j is over
+        jj <- bezier_intersect(b[[i]],b[[j]],'para') # jj: c(ess,tee)
+        crosspoint <- jj[2]  # crosspoint parameter for the OVERstrand
+        xy_thick <- rbind(xy_thick,bezier(b[[j]],tee=seq(from=crosspoint-0.3,to=crosspoint+0.3, length=n)),NA)
+      }
+      xy_thin <- rbind(xy_thin, bezier(b[[i]],tee=tee))
+    } # 'i' loop closes
+
+    points(xy_thin, type='l',lwd=lwd,              lend=1, ljoin=1,              ...)
+    points(xy_thick,type='l',lwd=lwd*(1+gapwidth), lend=1, ljoin=1, col="white", ...)
+    points(xy_thick,type='l',lwd=lwd,              lend=1, ljoin=1,              ...)
+
+    return(invisible(xy_thin))
+}
+
 
 b <- lapply(a,overunder)
 
@@ -87,7 +120,7 @@ for(i in 1:6){
         k[,2] <- k[,2] - mean(k[,2])
         k <- sweep(k,2,c(xoff,yoff),"+")
         print(n)
-        knotplot(k,b[[n]],add=TRUE,lwd=7,gap=3)
+        knotplot_bespoke(k,b[[n]],add=TRUE,lwd=7,gap=3)
         text(xoff+xtext,yoff+ytext,leg[[n]],
              cex=3)
     }
